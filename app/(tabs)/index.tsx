0f5770dd4c38c96bff29887665c61fa6be499725
PedroPsy <miguelkarate8@gmail.com>
2026-01-15 11:32:01 -0300

    melhorando o productcard

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,23 +12,25 @@ export default function Home() {
         <Hero />
 
         <View style={{ paddingHorizontal: 16 }}>
-          <ProductCard
-            name="iPhone 15 Pro"
-            description="O iPhone definitivo. Design em titânio. O chip mais poderoso."
-            image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_15_pro__f4p0p0xk6w2y_large.png"
-          />
+              <ProductCard
+                index={0}
+                name="iPhone 15 Pro"
+                description="O iPhone definitivo. Design em titânio. O chip mais poderoso."
+                image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_15_pro__f4p0p0xk6w2y_large.png"
+              />
+              <ProductCard
+                index={1}
+                name="iPhone 15"
+                description="Uma nova câmera de 48 MP. Design incrível. Tudo em um só."
+                image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_15__e0tw6kz6l4ya_large.png"
+              />
+              <ProductCard
+                index={2}
+                name="iPhone SE"
+                description="Potência incrível em um design clássico."
+                image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_se__cuaa2bdndqeu_large.png"
+              />
 
-          <ProductCard
-            name="iPhone 15"
-            description="Uma nova câmera de 48 MP. Design incrível. Tudo em um só."
-            image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_15__e0tw6kz6l4ya_large.png"
-          />
-
-          <ProductCard
-            name="iPhone SE"
-            description="Potência incrível em um design clássico."
-            image="https://www.apple.com/v/iphone/home/bu/images/overview/select/iphone_se__cuaa2bdndqeu_large.png"
-          />
         </View>
       </ScrollView>
     </View>
